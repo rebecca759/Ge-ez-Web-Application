e714ed3477f401f13fc1a9453f24c429fe08ea15
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2021-02-08 11:05:28 -0800

    add image paths to game.js to access them on the game

---
+++ assets/js/game.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+      {
+        name: 'fetere',
+        img: '../assets/images/fetere.jpg'
+      },
+      {
+        name: 'fetere',
+        img: '../assets/images/fetere.jpg'
+      },
+      {
+        name: 'nebere',
+        img: '../assets/images/nebere.jpg'
+      },
+      {
+        name: 'nebere',
+        img: '../assets/images/nebere.jpg'
+      },
+      {
+        name: 'hore',
+        img: '../assets/images/hore.jpg'
+      },
+      {
+        name: 'hore',
+        img: '../assets/images/hore.jpg'
+      },
+      {
+        name: 'temhre',
+        img: '../assets/images/temhre.jpg'
+      },
+      {
+        name: 'temhre',
+        img: '../assets/images/temhre.jpg'
+      },
+      {
+        name: 'hame',
+        img: '../assets/images/hame.jpg'
+      },
+      {
+        name: 'hame',
+        img: '../assets/images/hame.jpg'
+      },
+      {
+        name: 'aemere',
+        img: '../assets/images/aemere.jpg'
+      },
+      {
+        name: 'aemere',
+        img: '../assets/images/aemere.jpg'
+      }
+    ]
+  
+    
