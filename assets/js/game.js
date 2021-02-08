328d8ba8f36277d2fd4e579d1ebfb35eb116ad9b
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2021-02-08 11:07:59 -0800

    add function that check for match

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,4 +70,39 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   
+    //check for matches
+    function checkForMatch() {
+      const cards = document.querySelectorAll('img')
+      const optionOneId = cardsChosenId[0]
+      const optionTwoId = cardsChosenId[1]
+      
+      if(optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', '../assets/images/blank.png')
+        cards[optionTwoId].setAttribute('src', '../assets/images/blank.png')
+        alert('You have clicked the same image!')
+      }
+      else if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match')
+        cards[optionOneId].setAttribute('src', '../assets/images/white.png')
+        cards[optionTwoId].setAttribute('src', '../assets/images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+      } else {
+        cards[optionOneId].setAttribute('src', '../assets/images/blank.png')
+        cards[optionTwoId].setAttribute('src', '../assets/images/blank.png')
+        alert('Sorry, try again')
+      }
+      cardsChosen = []
+      cardsChosenId = []
+      resultDisplay.textContent = cardsWon.length
+      if  (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulations! You found them all!'
+      }
+    }
+  
     
+  
+    createBoard()
+  })
+  
