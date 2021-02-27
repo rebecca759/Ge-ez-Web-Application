8e047c47bf17ac55054505b4a574982f03ef5a3f
Ruth Matewos <se.ruth.matewos@gmail.com>
2021-02-28 01:51:29 +0300

    added a search for Geez verbs

--- assets/js/level2Db.js
+++ assets/js/level2Db.js
@@ -37,3 +37,63 @@ bulkcreate(db.verb, {
     verbW: "ነባሪያተ ፡ የተቀመጡ ሴቶች",
 })
 
+bulkcreate(db.verb, {
+    verbG: "ሖረ",
+    verbAm: "ሄደ",
+    verbQ: "ይሑር ፡  ይሂድ (ትዕዛዝ)",
+    verbA: "ሐዊር ፡ መሄድ",
+    verbB: "ሐዋሪ ፡ የሄደ ወንድ",
+    verbM: "ሐዋሪያን ፡ የሄዱ ወንዶች",
+    verbGi: "ሐዋሪተ ፡ የሄደች ሴት",
+    verbW: "ሐዋሪያት ፡ የሄዱ ሴቶች",
+})
+
+
+
+/// add even listner to the search 
+GverbButton.addEventListener('click', searchGeezVerb);
+//AverbButton.addEventListener('click', searchAmharicVerb);
+
+
+/// function for search using Geez word 
+
+function searchGeezVerb(){
+
+    //check if search input is empty
+    if (GverbInput.value == '') {
+        GverbInput.style.borderColor = "red";
+        return;//Avoiding else statement
+    }
+   
+    GverbInput.style.borderColor = '#ebebeb';
+
+    //get search letter input
+
+    var word = GverbInput.value;
+
+    if(word == "ነበረ") {
+        db.verb.get(1, function(rowOne) {
+            verbGeez.innerHTML = `${rowOne['verbG']} (ግስ) : ${rowOne['verbAm']} (የአማርኛ ፍቺ)`;
+            verbQuest.innerHTML = rowOne['verbQ'];
+            verbAction.innerHTML = rowOne['verbA'];
+            verbBoy.innerHTML = rowOne['verbB'];
+            verbMen.innerHTML = rowOne['verbM'];
+            verbGirl.innerHTML = rowOne['verbGi'];
+            verbWomen.innerHTML = rowOne['verbW'];
+            
+        })
+    } else if(word == "ሖረ") {
+        db.verb.get(2, function(rowTwo) {
+            verbGeez.innerHTML = `${rowTwo['verbG']} (ግስ) : ${rowTwo['verbAm']} (የአማርኛ ፍቺ)`;
+            verbQuest.innerHTML = rowTwo['verbQ'];
+            verbAction.innerHTML = rowTwo['verbA'];
+            verbBoy.innerHTML = rowTwo['verbB'];
+            verbMen.innerHTML = rowTwo['verbM'];
+            verbGirl.innerHTML = rowTwo['verbGi'];
+            verbWomen.innerHTML = rowTwo['verbW'];
+            
+        })
+    }
+
+    
+}
